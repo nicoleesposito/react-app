8341a6209409416bbd6881cd6d7526321eb5bd10
nicoleesposito <79225981+nicoleesposito@users.noreply.github.com>
2025-02-04 01:15:28 -0500

    React Application finished product

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,10 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
@@ -16,15 +16,27 @@ export default function Sidebar() {
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
     //   setMenuItems([item, ...menuItems])
-  }, [])
+    if (newMenuItem !== "") {
+      setMenuItems([newMenuItem, ...menuItems]);
+      setNewMenuItem("");
+    }
+  }, [newMenuItem, menuItems])
 
-  // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+  const menuItemsFiltered = menuItems.filter((item) => {
+    const regex = new RegExp(filter, 'i'); // case insensitive [i] through the use of regular expressions.
+    return item.match(regex) !== null;  // if theres a match it will return true
+  });
 
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
   return (
     <div>
+      <ul>
+        {/* maps over filtered menu items and display each item onto the page as a list item. key pop is added for DOM modification. react sees which items have been manipulated using this key */}
+        {menuItemsFiltered.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
       <input
         type="text"
         id="newMenuItemValue"
@@ -33,8 +45,8 @@ export default function Sidebar() {
       ></input>
       <br />
       <button
-        onClick={() => {
-          /* TODO: 3 */
+        onClick={() => { // on click of the button, the addMenuItem function will be called for adding new items.
+          addMenuItem();
         }}
       >
         Add Item
@@ -47,6 +59,8 @@ export default function Sidebar() {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+
     </div>
+
   )
 }
